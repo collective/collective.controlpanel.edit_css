3b89ed4fe6f8f167c9d0456a5fe57a3b60f0a2b6
aclark <aclark@localhost>
2009-10-21 16:29:52 +0000

    Edit assinine description to be less crappy
    svn path=/collective.controlpanel.edit_css/trunk/; revision=100010

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.2'
 
 setup(name='collective.controlpanel.edit_css',
       version=version,
-      description="Use a text area in the Plone control panel to save *just* CSS (no dtml because now-a-days I just find it confusing and unnecessary) to a page template called ploneCustom.css (optionallly  allow filename to be specified e.g. my_theme.css) in portal_skins/custom, for quick and dirty UI prototyping.",
+      description="Use a text area in the Plone control panel to edit CSS and Javascript.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -16,7 +16,7 @@ setup(name='collective.controlpanel.edit_css',
       keywords='plone theme',
       author='Alex Clark',
       author_email='aclark@aclark.net',
-      url='http://svn.plone.org/svn/collective/collective.controlpanel.edit_css/',
+      url='http://plone.org/products/collective.controlpanel.edit_css',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.controlpanel'],
