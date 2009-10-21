4f3d95d491f9309afaaff8b6084bf57358217558
aclark <aclark@localhost>
2009-10-21 11:34:37 +0000

    Pet peeve variable and function renaming.
    svn path=/collective.controlpanel.edit_css/trunk/; revision=99964

--- collective/controlpanel/edit_css/browser/edit_css.py
+++ collective/controlpanel/edit_css/browser/edit_css.py
@@ -9,7 +9,7 @@ class EditCSS(BrowserView):
 
     template = ViewPageTemplateFile('edit_css.pt')
     default_text = u'/* DELETE THIS LINE AND PUT YOUR CUSTOM STUFF HERE */'
-    customcss = 'ploneCustom.css'
+    custom_css = 'ploneCustom.css'
 
     def __call__(self, *args, **kw):
         request = self.context.request
@@ -25,25 +25,25 @@ class EditCSS(BrowserView):
     def getPloneCustom(self):
         site = getSite()
         skins = site.portal_skins
-        if self.customcss in skins.custom.objectIds():
+        if self.custom_css in skins.custom.objectIds():
             try:
-                return skins.custom[self.customcss].document_src()
+                return skins.custom[self.custom_css].document_src()
             except:
                 raise Exception("It's not possible to manage the object %s. "
-                    "Use '%s' instead." % (repr(skins.custom[self.customcss]),
+                    "Use '%s' instead." % (repr(skins.custom[self.custom_css]),
                     "ZopePageTemplate"))
         else:
-            id = self.customcss
+            id = self.custom_css
             text = self.default_text
             content_type = 'text/html'
             obj = ZopePageTemplate(id, text, content_type)
             skins.custom._setObject(id, obj)
-            return skins.custom[self.customcss].document_src()
+            return skins.custom[self.custom_css].document_src()
 
     def setPloneCustom(self, text):
         site = getSite()
         skins = site.portal_skins
-        obj = skins.custom[self.customcss]
+        obj = skins.custom[self.custom_css]
         try:
             if hasattr(obj, 'manage_edit'):
                 # DTMLMethod
@@ -54,16 +54,16 @@ class EditCSS(BrowserView):
         except:
             raise Exception("It's not possible to update object %s. "
                 "Use '%s' instead." % (repr(obj), "ZopePageTemplate"))
-        self.updatePloneCSS()
+        self.updatePloneCustom()
 
-    def updatePloneCSS(self):
+    def updatePloneCustom(self):
         site = getSite()
         cssreg = getToolByName(site, 'portal_css', None)
         if cssreg is not None:
             stylesheet_ids = cssreg.getResourceIds()
-            if self.customcss not in stylesheet_ids:
+            if self.custom_css not in stylesheet_ids:
                 cssreg.registerStylesheet(id=sheetId, enabled=True)
                 cssreg.cookResources()
             else:
-                cssreg.updateStylesheet(self.customcss, enabled=True)
+                cssreg.updateStylesheet(self.custom_css, enabled=True)
                 cssreg.cookResources()
